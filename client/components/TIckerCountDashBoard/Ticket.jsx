345131e870025ad3f4c372becd2baee6cd22a8b4
rishiTamoghna17 <tamoghna171099@gmail.com>
2023-07-28 19:09:24 +0530

    "ticket problem"

--- client/components/TIckerCountDashBoard/Ticket.jsx
+++ client/components/TIckerCountDashBoard/Ticket.jsx
@@ -4,20 +4,12 @@ import Chart from "react-apexcharts";
 import useFetch from "../../hooks/useFetch";
 import { AiOutlineMail } from "react-icons/ai";
 
-const dummyTicketData = [
-  // Replace this with your actual dummy data for tickets
-  // Dummy data should contain objects with 'date' and 'count' properties
-  { date: "2023-06-15", count: 10 },
-  { date: "2023-06-16", count: 15 },
-  { date: "2023-06-17", count: 20 },
-  { date: "2023-06-18", count: 10 },
-  { date: "2023-06-19", count: 15 },
-  { date: "2023-06-20", count: 25 },
-  // Add more data as needed
-];
-
-function Ticket({graph}) {
+function Ticket({ graph }) {
+  const [selectedAutomationType, setSelectedAutomationType] = useState("all");
+  const [selectedDateFilter, setSelectedDateFilter] = useState("last30days");
   const [ticket, setTicket] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [data, setData] = useState([]);
   const fetch = useFetch();
   const [chartData, setChartData] = useState({
     options: {
@@ -57,7 +49,7 @@ function Ticket({graph}) {
       tooltip: {
         enabled: true,
         show: true,
-        custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+        custom: function ({ series, seriesIndex, dataPointIndex }) {
           return `<div class="custom-tooltip">${series[seriesIndex][dataPointIndex]}</div>`;
         },
       },
@@ -70,14 +62,11 @@ function Ticket({graph}) {
     ],
   });
 
-  const [selectedAutomationType, setSelectedAutomationType] = useState("all");
-  const [selectedDateFilter, setSelectedDateFilter] = useState("last30days");
-
-  // Fetch ticket data based on filters
   async function fetchContent() {
     setTicket("loading...");
     const res = await fetch("/api/tickets");
     const response = await res.json();
+    console.log(response);
     if (response.error) {
       setTicket([]);
     } else {
@@ -94,7 +83,7 @@ function Ticket({graph}) {
 
   // Update chart data based on selected filters
   function updateChartData() {
-    let filteredData = [...dummyTicketData];
+    let filteredData = [...ticket];
 
     // Filter by automation type
     if (selectedAutomationType !== "all") {
@@ -107,29 +96,74 @@ function Ticket({graph}) {
     switch (selectedDateFilter) {
       case "last90days":
         // Implement filtering for last 90 days
+        const last90Days = new Date();
+        last90Days.setDate(last90Days.getDate() - 90);
+        filteredData = filteredData.filter(
+          (item) => new Date(item.created_at) >= last90Days
+        );
         break;
       case "last60days":
         // Implement filtering for last 60 days
+        const last60Days = new Date();
+        last60Days.setDate(last60Days.getDate() - 60);
+        filteredData = filteredData.filter(
+          (item) => new Date(item.created_at) >= last60Days
+        );
         break;
       case "last30days":
         // Implement filtering for last 30 days
+        const last30Days = new Date();
+        last30Days.setDate(last30Days.getDate() - 30);
+        filteredData = filteredData.filter(
+          (item) => new Date(item.created_at) >= last30Days
+        );
         break;
       case "last15days":
         // Implement filtering for last 15 days
+        const last15Days = new Date();
+        last15Days.setDate(last15Days.getDate() - 15);
+        filteredData = filteredData.filter(
+          (item) => new Date(item.created_at) >= last15Days
+        );
         break;
       case "lastweek":
         // Implement filtering for last week
+        const lastWeek = new Date();
+        lastWeek.setDate(lastWeek.getDate() - 7);
+        filteredData = filteredData.filter(
+          (item) => new Date(item.created_at) >= lastWeek
+        );
         break;
       case "custom":
         // Implement filtering for custom date range
+        // Replace 'startDate' and 'endDate' with actual custom date range values
+        const startDate = new Date("2023-07-01");
+        const endDate = new Date("2023-07-28");
+        filteredData = filteredData.filter(
+          (item) =>
+            new Date(item.created_at) >= startDate &&
+            new Date(item.created_at) <= endDate
+        );
         break;
       default:
         break;
     }
 
-    // Update chartData.series and chartData.options.xaxis.categories accordingly
-    const categories = filteredData.map((item) => item.date);
-    const data = filteredData.map((item) => item.count);
+    const ticketCountByDate = {};
+    filteredData.forEach((item) => {
+      const date = item.created_at.split("T")[0]; // Extract date part from "created_at"
+
+      if (ticketCountByDate[date]) {
+        ticketCountByDate[date] += 1; // Increment ticket count for the date
+      } else {
+        ticketCountByDate[date] = 1; // Initialize ticket count for the date
+      }
+    });
+
+    // Extract the dates (categories) and ticket counts (data) from the object
+
+    setCategories(Object.keys(ticketCountByDate));
+    setData(Object.values(ticketCountByDate));
 
     setChartData((prevChartData) => ({
       ...prevChartData,
@@ -154,45 +188,59 @@ function Ticket({graph}) {
     updateChartData();
   }, [selectedAutomationType, selectedDateFilter]);
 
+  console.log("categories", categories);
+  console.log("data", data);
+
   return (
     <div className="ticket-count-dashboard">
-      <div className="ticket-count-allignment">
-        <div className="ticket-count-heading">
-          <h1>Total Tickets</h1> <br />{" "}
-          <span className="total-ticket-count">{totalTicketCount}</span>
-        </div>
-        <div className="ticket-filters">
-          {/* Implement automation type filter */}
-          <select
-            value={selectedAutomationType}
-            onChange={(e) => setSelectedAutomationType(e.target.value)}
-          >
-            <option value="all">All</option>
-            <option value="email">Email </option>
-            <option value="frontend">Frontend </option>
-          </select>
-          {/* Implement date filter */}
-          <select
-            value={selectedDateFilter}
-            onChange={(e) => setSelectedDateFilter(e.target.value)}
-          >
-            <option value="last90days">Last 90 Days</option>
-            <option value="last60days">Last 60 Days</option>
-            <option value="last30days">Last 30 Days</option>
-            <option value="last15days">Last 15 Days</option>
-            <option value="lastweek">Last Week</option>
-            <option value="custom">Custom Date Range</option>
-          </select>
-        </div>
-      </div>
-      <div className={graph==true?"ticket-graph":"ticket-disabled"}>
-        <Chart
-          options={chartData.options}
-          series={chartData.series}
-          type="line"
-          height={150}
-        />
-      </div>
+      {/* Show loading message while data is being fetched */}
+      {ticket === "loading..." ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <div className="ticket-count-allignment">
+            <div className="ticket-count-heading">
+              <h1>Total Tickets</h1> <br />
+              <span className="total-ticket-count">{totalTicketCount}</span>
+            </div>
+            <div className="ticket-filters">
+              {/* Implement automation type filter */}
+              <select
+                value={selectedAutomationType}
+                onChange={(e) => setSelectedAutomationType(e.target.value)}
+              >
+                <option value="all">All</option>
+                <option value="email">Email</option>
+                <option value="frontend">Frontend</option>
+              </select>
+              {/* Implement date filter */}
+              <select
+                value={selectedDateFilter}
+                onChange={(e) => setSelectedDateFilter(e.target.value)}
+              >
+                <option value="last90days">Last 90 Days</option>
+                <option value="last60days">Last 60 Days</option>
+                <option value="last30days">Last 30 Days</option>
+                <option value="last15days">Last 15 Days</option>
+                <option value="lastweek">Last Week</option>
+                <option value="custom">Custom Date Range</option>
+              </select>
+            </div>
+          </div>
+          <div className={graph ? "ticket-graph" : "ticket-disabled"}>
+            {categories?.length === 0 || data?.length === 0 ? (
+              <div>No data available</div>
+            ) : (
+              <Chart
+                options={chartData.options}
+                series={chartData.series}
+                type="line"
+                height={150}
+              />
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -19,7 +19,7 @@ userRoutes.post("/api", (req, res) => {
 //******************************************************************************************* */
 
 //create tickets
-userRoutes.post('/api/tickets', async (req, res) => {
+userRoutes.post("/api/tickets", async (req, res) => {
   try {
     const { subject, description, requester, priority, support } = req.body;
 
@@ -28,32 +28,30 @@ userRoutes.post('/api/tickets', async (req, res) => {
       description,
       requester,
       priority,
-      support
+      support,
     });
 
     await ticket.save();
 
     res.status(201).json({ ticket });
   } catch (error) {
-    res.status(500).json({ error: 'Failed to create a ticket' });
+    res.status(500).json({ error: "Failed to create a ticket" });
   }
 });
 // Get all tickets
-userRoutes.get('/api/tickets', async (req, res) => {
-  try {
-    const tickets = await Ticket.find();
-    res.status(200).send(tickets);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to get the tickets' });
-  }
-});
+// userRoutes.get("/api/tickets", async (req, res) => {
+//   try {
+//     const tickets = await Ticket.find();
+//     res.status(200).send(tickets);
+//   } catch (error) {
+//     res.status(500).json({ error: "Failed to get the tickets" });
+//   }
+// });
 
 //create zendesk ticket api
-userRoutes.post('/api/createZendeskTicket', (req, res) => {
-
+userRoutes.post("/api/createZendeskTicket", (req, res) => {
   const { subject, description, requesterName, requesterEmail } = req.body;
 
-  // Prepare the ticket data
   const ticketData = {
     ticket: {
       subject,
@@ -64,12 +62,10 @@ userRoutes.post('/api/createZendeskTicket', (req, res) => {
       },
     },
   };
-
-  // Create the ticket
   zd.tickets.create(ticketData, (err, req, result) => {
     if (err) {
-      console.error('Error creating Zendesk ticket:', err);
-      res.status(500).json({ error: 'Failed to create Zendesk ticket' });
+      console.error("Error creating Zendesk ticket:", err);
+      res.status(500).json({ error: "Failed to create Zendesk ticket" });
     } else {
       // console.log('Zendesk ticket created:', result);
       res.status(201).json({ success: true });
@@ -77,6 +73,20 @@ userRoutes.post('/api/createZendeskTicket', (req, res) => {
   });
 });
 
+// GET endpoint to retrieve all tickets
+userRoutes.get('/api/tickets', (req, res) => {
+  // Use the 'tickets' API to get all tickets
+  zd.tickets.list((err, req, result) => {
+    if (err) {
+      console.error('Error fetching tickets:', err);
+      res.status(500).json({ error: 'Failed to fetch tickets' });
+    } else {
+      const tickets = result 
+      // console.log('All tickets:', JSON.stringify(result.slice(0,2)));
+      res.status(200).json(tickets);
+    }
+  });
+});
 
 //****************************************************************//
 
